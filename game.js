2535180757b737871a3fbf2e3664976e69ee92e2
Calvin Merry <calvin@calvinmerry.com>
2023-05-26 10:11:54 +0100

    Remove some unused code (e.g. DOM elements, function calls)

--- game.js
+++ game.js
@@ -2,9 +2,6 @@ import { Game, Character } from './classes.js';
 import { allRooms } from './objects.js';
 
 const displayText = document.getElementById("displaytext");
-const gameResponseText = document.getElementById('gameresponse');
-const userText = document.getElementById("userInput");
-const options = document.getElementById("options");
 const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
@@ -14,26 +11,7 @@ const option4 = document.getElementById("option4");
 let player = new Character('Player Name', 100, []);
 let game = new Game(player, allRooms);
 
-console.log(player);
-console.log(game);
-
-// function animateTyping(text, element) {
-//   element.textContent = '';
-
-//   const delay = 70; // Adjust the typing speed by changing this value
-//   let i = 0;
-
-//   function typeNextCharacter() {
-//     if (i < text.length) {
-//       element.textContent += text.charAt(i);
-//       i++;
-//       setTimeout(typeNextCharacter, delay);
-//     }
-//   }
-
-//   typeNextCharacter();
-// }
-
+// Displays the current room and text to the user
 function updateUI() {
   function displayRoom() {
     let roomName = game.currentRoom.returnName();
@@ -41,16 +19,10 @@ function updateUI() {
     let roomItems = game.currentRoom.returnItems();
     let roomEnemy = game.currentRoom.returnEnemy();
 
-
-    setTimeout(() => {
-      displayText.innerHTML = `You are in the ${roomName}. ${roomDescription}. You see ${roomItems}. There is ${roomEnemy.description}.`;
-    }, 200)
+    displayText.innerHTML = `You are in the ${roomName}. ${roomDescription}. You see ${roomItems}. There is ${roomEnemy.description}.`;
 
   }
-
   displayRoom();
-
-
 
   function displayOptions() {
 
@@ -73,35 +45,36 @@ function updateUI() {
   displayOptions();
 
 }
-
 updateUI();
 
+// Handles the user input and runs the selected option (1, 2, 3, 4)
 function userInput() {
   let inputField = document.getElementById('userInput');
 
   inputField.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
+      // Stops the page from reloading on enter
       event.preventDefault();
 
+      // Get the user input and clear the input field
       let userInput = inputField.value;
       inputField.value = '';
-
       userInput = Number(userInput);
 
-      // Get the options from the current room.
+      // Get the options from the current room
       let roomOptions = game.currentRoom.returnOptions();
 
       // Find the selected option based on input value.
       let selectedOption = roomOptions.find(option => option.input === userInput);
 
-      // Check if the selected option exists.
+      // Check if the selected option exists (if it is 1, 2, 3, 4)
       if (selectedOption && !selectedOption.text.includes("<s>")) {
         // Execute the selected option's action and pass the game object.
         selectedOption.action(game);
 
         // Update the UI after performing the action.
-
         updateUI();
+
       } else {
         console.error("Invalid input. Please enter a valid option number.");
       }
@@ -111,6 +84,3 @@ function userInput() {
 
 userInput();
 
-console.log(game._currentRoom)
-console.log(game._player);
-console.log(game._player._inventory);
